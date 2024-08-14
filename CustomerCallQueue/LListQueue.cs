98e8404238a489933f4a2603e30602a67db18eba
benji0295 <58788309+benji0295@users.noreply.github.com>
2024-08-14 13:31:43 -0400

    Add project files.

---
+++ CustomerCallQueue/LListQueue.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerCallQueue
+{
+    class Node
+    {
+        public int id;
+        public string name;
+        public Node next;
+        public Node(int id, string name, Node next)
+        {
+            this.id = id;
+            this.name = name;
+            this.next = next;
+        }
+    }
+    internal class LListQueue
+    {
+        Node front;
+        Node rear;
+        int size;
+
+        public LListQueue()
+        {
+            front = null;
+            rear = null;
+            size = 0;
+        }
+        public bool IsEmpty()
+        {
+            return size == 0;
+        }
+        public void Enqueue(int id, string name)
+        {
+            Node newest = new Node(id, name, null);
+            if (IsEmpty())
+            {
+                front = newest;
+            }
+            else
+            {
+                rear.next = newest;
+            }
+            rear = newest;
+            size++;
+        }
+        public int Dequeue()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The queue is empty.");
+                return -1;
+            }
+            int val = front.id;
+            front = front.next;
+            size--;
+            if (IsEmpty())
+            {
+                rear = null;
+            }
+            return val;
+        }
+        public void Display()
+        {
+            Node temp = front;
+            if (IsEmpty())
+            {
+                Console.WriteLine("The queue is empty.");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine($"Caller ID: {temp.id}, Name: {temp.name}");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ CustomerCallQueue/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerCallQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LListQueue callQueue = new LListQueue();
+            int callerId = 1;
+            int input = 0;
+            while (input != 4)
+            {
+                Console.WriteLine("1: View current call queue | 2: Add customer to queue | 3: Remove customer to queue | 4: Exit");
+                input = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+                switch (input)
+                {
+                    case 1:
+                        callQueue.Display();
+                        break;
+                    case 2:
+                        Console.Write("Enter the customer name: ");
+                        string name = Console.ReadLine();
+                        callQueue.Enqueue(callerId, name);
+                        callerId++;
+                        break;
+                    case 3:
+                        callQueue.Dequeue();
+                        break;
+                    case 4:
+                        Console.WriteLine("Goodbye!");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
